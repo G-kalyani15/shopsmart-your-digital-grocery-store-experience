61dab92cd4faee2e869316c1150692cb9372133e
G-kalyani15 <gkalyani334@gmail.com>
2025-07-03 17:59:07 +0530

    Add files via upload

--- projectfiles/frontend/index.js
+++ projectfiles/frontend/index.js
@@ -1,7 +1,55 @@
-import { NotFoundContainer } from "./styledComponents"
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import Cookies from 'js-cookies'
+import {
+  ProductContainer,
+  ProductName,
+  ProductDescription,
+  ProductPrice,
+  ProductImage,
+  Button,
+  ButtonContainer,
+} from "./styledComponents";
 
-const NotFound = () => (
-    <NotFoundContainer>Not Found</NotFoundContainer>
-)
+const ProductItem = ({ id, name, description, price, img }) => {
 
-export default NotFound
+  const handleAddToCart = async () => {
+    const userId = Cookies.getItem("userId"); // Get the user ID from cookies or your authentication system
+
+    try {
+      const response = await axios.post("http://localhost:5100/add-to-cart", {
+        userId,
+        productId: id,
+      });
+
+      // Handle success here, you can show a success message or update the UI.
+      alert('Product Added to cart!');
+    } catch (error) {
+      console.error("Error adding to cart:", error);
+      // Handle error here, show an error message or log it.
+    }
+  };
+
+  return (
+    <ProductContainer>
+      <ProductImage src={img} alt={name} />
+      <ProductName>{name}</ProductName>
+      <ProductPrice>${price}</ProductPrice>
+      <ButtonContainer>
+        <Link to={`/order-details/${id}`} className="btn btn-primary" style={{borderRadius:'0'}}>Buy Now</Link>
+        <Button onClick={handleAddToCart}>Add to Cart</Button>
+      </ButtonContainer>
+      {/* <div>
+        <label>Quantity: </label>
+        <input
+          type="number"
+          value={quantity}
+          onChange={(e) => setQuantity(e.target.value)}
+        />
+      </div> */}
+    </ProductContainer>
+  );
+};
+
+export default ProductItem;
